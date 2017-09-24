5c3077d688d35e18bb539b01cbfb72b1832cad8c
Nat Tuck <ntuck@ccs.neu.edu>
2017-09-24 10:14:16 -0400

    Add server: true to production config.

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :nu_mart, NuMartWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  server: true,
+  url: [host: "webshop.ironbeard.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
